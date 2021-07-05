14006f39626155a238db7e911a916817d87ddc98
lileilei <lileilei@ewell.cc>
2021-07-05 10:43:06 +0800

    html模版插件

--- vite-react/plugins/vite-plugin-transform-html.js
+++ vite-react/plugins/vite-plugin-transform-html.js
@@ -12,9 +12,13 @@ return `
     </script>`
 }
 export default function htmlPlugin() {
+  let isProd = process.env.NODE_ENV === 'production'
   return {
     name: 'html-transform',
     transformIndexHtml(html) {
+      if(!isProd){
+        html =  html.replace(/(react(-dom)?).production.min/g,`$1.development`)
+      }
       html =  html.replace(/<script.*type="module".*src=\"(.*)\"><\/script>/,transformHtml(`$1`))
       return html
     }
